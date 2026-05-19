0ae0c7fbbcf8849a30f2b0d5a5f74c97e81a8c8e
bren <b.ciano.549055@umindanao.edu.ph>
2026-05-19 11:39:42 +0800

    GogoGAGA

---
+++ models/account.php
@@ -0,0 +1,27 @@
+<?php
+
+class Account {
+
+    // Properties
+    public $id="";
+    public $username="";
+    public $password="";
+    public $created_at="";
+
+    function __construct(
+        $username,
+        $password,
+        $created_at="",
+        $id=""
+    ){
+
+        $this->id=$id;
+        $this->username=$username;
+        $this->password=$password;
+        $this->created_at=$created_at;
+
+    }
+
+}
+
+?>
